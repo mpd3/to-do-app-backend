fcbb392c88da08c1200dbd1875e59c65bb29d73b
Mike D'Antonio <mikedantonio56@gmail.com>
2017-06-24 18:19:41 +0000

    Added number for show tab

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,8 +3,20 @@ class TodoController < ApplicationController
             @math = 4 + 2
     end
     def show
-        @todo_description = "Walk the dog"
-        @pomodoros = "4"
         @id = params[:id]
+        if @id == '1'
+            puts "hey there"
+            @todo_description = "Walk the dog"
+            @pomodoros = "4"
+
+        elsif @id == '2'
+            puts "hi"
+            @todo_description = "CLean my room"
+            @pomodoros = "3"
+        else
+            puts "bye"
+            @todo_description = "Eat"
+            @pomodoros = "1"
+        end
     end
 end
