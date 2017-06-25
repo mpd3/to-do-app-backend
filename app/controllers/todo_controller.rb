8d6c40d97702341bed8b331242a11c1df433043f
Mike D'Antonio <mikedantonio56@gmail.com>
2017-06-25 18:54:13 +0000

    fixed delete buttons and edit

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,32 +1,33 @@
 class TodoController < ApplicationController    
     def index 
-            @math = 4 + 2
+            @todos=Todo.all
+            
     end
     def show
-        @todo= Todo.find(params[:id])
+        @todo= Todo.find_by_id(params[:id])
     end
     def new
     end 
     def create
         t = Todo.new
         t.description = params['description']
-        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.pomodoro_estimate = params['pomodoro-estimate']
         t.save
         redirect_to "/todo/show/#{ t.id }"
     end
     def edit
-         @todo= Todo.find(params[:id])
+         @todo = Todo.find_by_id(params[:id])
     end
     def update
-        t = Todo.find(params[:id])
+        t = Todo.find_by_id(params[:id])
         t.description = params['description']
-        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.pomodoro_estimate = params['pomodoro-estimate']
         t.save
         redirect_to "/todo/show/#{ t.id }"
     end    
     def destroy
-        t = Todo.find(params[:id])
+        t = Todo.find_by(id: params[:id])
         t.destroy
-        redirect_to "todo/index"
+        redirect_to "/todo/index"
     end
 end
